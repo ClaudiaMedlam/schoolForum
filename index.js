24dc804c682f54f41f13c414aa4faafb8c7aeb3b
ClaudiaMedlam <claudiamedlam@gmail.com>
2023-12-13 15:29:14 +0000

    Version 13.12 passport

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var ejs = require('ejs')
 var mysql = require('mysql');
 const bodyParser = require('body-parser');
 
+// For login/logout authentication:
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 // Creates the express application object
 const app = express()
 const port = 8000
@@ -42,13 +46,56 @@ app.set('view engine', 'ejs');
 // Tells Express to process html files using EJS's rendering engine
 app.engine('html', ejs.renderFile);
 
+// Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport local strategy setup
+passport.use(new LocalStrategy((username, password, done) => {
+    // Check username and password in the database
+    let sqlquery = `SELECT * FROM users WHERE user_name = ?`;
+    
+    db.query(sqlquery, [req.body.user_name], (err, result) => {
+        if(err) {
+            return console.log.error(err);
+        }    
+
+        if(result.length==0) {
+            return done(null, false, {message: "Incorrect username."});
+        }
+
+        let user = result[0];
+
+        // Compare passwords with database
+        if(password == user.password) {
+            return done(null, user);
+
+        }
+        else {
+            return done(null, false, {message: "Incorrect password."});
+        }
+    });
+}));
+
+// Serialise and desearialise user (as per Brown (2020) p.243)
+passport.serializeUser((user, done) => {
+    done(null, user.user_id);
+
+})
+
+passport.deserializeUser((user_id, done) => {
+    let sqlquery = `SELECT * FROM users WHERE user_id = ?`;
+    
+    db.query(sqlquery, [user_id], (err, result) => {
+        if(err) throw err;
+        let user = result[0];
+        done(null, user);
+    });
+});
 
 // Requires the main.js file inside the routes folder passing in the Express app
 // and data as arguments.  All the routes are found in this file
 require('./routes/main')(app, websiteData);
-
-
-
 
 
 // Starts the web app listening
--- routes/main.js
+++ routes/main.js
@@ -25,7 +25,6 @@ module.exports = function(app, websiteData) {
 
             // Creates object combining websiteData and database topics
             let newData = Object.assign({}, websiteData, {availableTopics:result});
-            console.log(newData.name);
             res.render('topics.ejs', newData);
 
         });
@@ -45,7 +44,6 @@ module.exports = function(app, websiteData) {
 
             // Creates object combining websiteData and database topics
             let newData = Object.assign({}, websiteData, {allUsers:result});
-            console.log(newData.name);
             res.render('users.ejs', newData);
 
         });
@@ -73,7 +71,7 @@ module.exports = function(app, websiteData) {
                     month: 'short',
                     year: '2-digit'
                 });
-    }
+            }
             // Format the date in the result
             const formattedResult = result.map(post => {
             return { ...post, post_date: formatDate(post.post_date) };
@@ -88,31 +86,78 @@ module.exports = function(app, websiteData) {
 
     // ADD NEW POST PAGE
     app.get('/addposts', function(req, res) {
-        res.render('addposts.ejs', websiteData)
+        // Queries database to get all the users
+        let sqlquery = "SELECT * FROM topics"
+
+        // Executes sql query
+        db.query(sqlquery, (err, result) => {
+            // Redirects to home page in case of any database query error
+            if (err) {
+                res.redirect('./');
+            }
+
+            // Creates object combining websiteData and database topics
+        // Creates object combining websiteData and database topics
+        let newData = Object.assign({}, websiteData, {availableTopics:result});
+        res.render('addposts.ejs', newData)
+        });
+    
     });
 
     app.post('/added-post', function(req, res) {
-        // Saves data to database
-        let sqlquery = `INSERT INTO posts (title, content, user_id, topic_id, datePosted) 
-                        VALUES (?,?,?,?,?)`; 
+        let user_id = -1;
+        let topic_id = -1;
 
-        // Prepares the data for query
-        let newrecord = [req.body.title, req.body.content, req.body.user_id, req.body.topic_id, req.body.datePosted];
-        // Executes sql query
-        db.query(sqlquery, newrecord, (err, result) => {
-            if (err) {
+        // Log the received form data
+        console.log("Received form data:", req.body);
+
+        // Get user id from the user name:
+        let sqlquery = `SELECT * FROM users WHERE user_name = ?`
+        db.query(sqlquery, [req.body.user_name], (err, result) => {
+            if(err) {
                 return console.error(err.message);
             }
-            
-            // Confirmation message
-            else {
-                res.send("This post has been added to database: title: " + 
-                req.body.title + ",   content: " + req.body.content);
+
+            if (result.length==0) {
+                console.log("User is not found")
 
             }
 
-        });
+            user_id = result[0].user_id;
+            console.log("User is " + user_id);
 
+            // Get the topic id from the topic title
+            sqlquery = `SELECT * FROM topics WHERE topic_title = ?`
+            db.query(sqlquery, [req.body.topic_title], (err, result) => {
+                if(err) {
+                    return console.error(err.message);
+                }
+
+                topic_id = result[0].topic_id;
+                console.log("Topic is " + topic_id);
+                
+                // Insert the post, save data to database
+                sqlquery = `INSERT INTO posts (post_date, post_title, post_content, user_id, topic_id)
+                            VALUES (now(), ?, ?, ?, ? )`
+
+                // Prepares the data for query
+                let newrecord = [req.body.post_title, req.body.post_content, user_id, topic_id];
+
+                // Executes sql query
+                db.query(sqlquery, newrecord, (err, result) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+            
+                // Confirmation message
+                else {
+                    res.send("Your post has been added to the forum");
+
+                }
+                })
+            })
+
+        });
     });
 
     // SEARCH POSTS
@@ -124,7 +169,7 @@ module.exports = function(app, websiteData) {
     // Search results return posts that contain the keyword entered in /search
     app.get('/search-result', function (req, res) { 
         // Searching the database
-        let sqlquery = "SELECT title, content, user_id FROM posts WHERE title LIKE ?";
+        let sqlquery = "SELECT post_title, post_content, user_id FROM posts WHERE post_title LIKE ?";
        
         // Execute sql query with wildcards to perform a case-insensitive partial match
         let searchRecord = ['%' + req.query.keyword + '%'];
