648f1477dac9637b8c84d42b60fe90f1410e41b7
ClaudiaMedlam <claudiamedlam@gmail.com>
2023-12-16 16:27:28 +0000

    16.12.23 with updated sql

--- create_db.sql
+++ create_db.sql
@@ -5,10 +5,29 @@ CREATE DATABASE schoolNet;
 USE schoolNet;
 
 --# Create the tables
-CREATE TABLE topics (topic_id INT AUTO_INCREMENT,name VARCHAR(150),unsigned,PRIMARY KEY(id));
-CREATE TABLE users (user_id INT AUTO_INCREMENT,name VARCHAR(50),email VARCHAR(50), password VARCHAR(20), unsigned,PRIMARY KEY(id));
-CREATE TABLE posts (post_id INT AUTO_INCREMENT, title VARCHAR(255), content VARCHAR(600), datePosted DATETIME(CURRENT_TIMESTAMP), user_id INT, topic_id INT, unsigned, PRIMARY KEY(id))
-CREATE TABLE userTopic (user_id, topid_id)
+CREATE TABLE topics (topic_id INT AUTO_INCREMENT, topic_title VARCHAR(150), PRIMARY KEY(topic_id));
+
+CREATE TABLE users (user_id INT AUTO_INCREMENT, user_name VARCHAR(50), user_password VARCHAR(20), user_email VARCHAR(50), PRIMARY KEY(user_id));
+
+CREATE TABLE posts(
+    -> post_id INT AUTO_INCREMENT,
+    -> post_title MEDIUMTEXT,
+    -> post_content LONGTEXT,
+    -> post_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    -> user_id INT,
+    -> topic_id INT,
+    -> PRIMARY KEY (post_id),
+    -> FOREIGN KEY (user_id) REFERENCES users(user_id),
+    -> FOREIGN KEY (topic_id) REFERENCES topics(topic_id)
+    -> );
+
+CREATE TABLE userTopic (
+    -> user_id INT,
+    -> topic_id INT,
+    -> PRIMARY KEY (user_id, topic_id),
+    -> FOREIGN KEY (user_id) REFERENCES users(user_id),
+    -> FOREIGN KEY (topic_id) REFERENCES topics(topic_id)
+    -> );
 
 --# Create the app user and give it access to the database
 CREATE USER 'appuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'app2027';
--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,27 +2,42 @@
 
 USE schoolNet;
 
-INSERT INTO topics (name) 
-VALUES('Uniform'), ('Trips'), ('Lunch'), ('Assemblies') ;
-
-INSERT INTO users (name, email, password) 
+INSERT INTO users (user_name, user_email, user_password) 
 VALUES('Anna', 'anna@karenina.com', 'p4ssword'), 
 ('Bilbo', 'bilbo@baggins.com', 'passw0rd'), 
 ('Cruella','cruella@devil.com','pa55word'), 
 ('David', 'david@copperfield.com', 'password!'),
-('Emma', 'emma@bovary.com', 'Password') ;
+('Emma', 'emma@bovary.com', 'Password'),
+('Llewelyn', 'Llewelyn.Fernandes@gold.ac.uk', 'DWApps') ;
 
 INSERT INTO topics (topic_title)
 VALUES ('Uniform'), ('Trips'), ('Lunch'), ('Assemblies'),
 ('Reception Year'), ('Year 1'), ('Year 2'), ('Year 3'), ('Year 4'), ('Year 5'), ('Year 6'), 
 ('School Socials'), ('Projects');
 
+-- #CREATE VIEWCREATE VIEW vw_posts AS
+SELECT
+    p.post_id,
+    p.post_date,
+    u.user_name,
+    t.topic_title,
+    p.post_title,
+    p.post_content
+FROM
+    posts p
+JOIN
+    users u ON u.user_id = p.user_id
+JOIN
+    topics t ON t.topic_id = p.topic_id
+ORDER BY
+    post_date DESC;
+
 
 -- # Creating sql stored procedure:
 DELIMITER //
-CREATE PROCEDURE sp_insert_post(IN p_post_title MEDIUMTEXT, IN p_post_content LONGTEXT, in p_topic_title VARCHAR(150), IN p_user_name VARCHAR(50))
+CREATE PROCEDURE sp_insert_post(IN p_post_title MEDIUMTEXT, IN p_post_content LONGTEXT, IN p_topic_title VARCHAR(150), IN p_user_name VARCHAR(50))
 BEGIN
-	DECLARE v_user_id INT;
+    DECLARE v_user_id INT;
     DECLARE v_topic_id INT;
     DECLARE v_user_is_member INT;
     
@@ -32,8 +47,8 @@ BEGIN
     INTO v_user_id;
     
     IF ISNULL(v_user_id) THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "No matching username found";
-	END IF;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No matching username found';
+    END IF;
     
     SELECT topic_id
     FROM topics
@@ -41,19 +56,20 @@ BEGIN
     INTO v_topic_id;
     
     IF ISNULL(v_topic_id) THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No matching topic found';
-	END IF;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No matching topic found';
+    END IF;
     
     SELECT COUNT(*) AS countmembership
     FROM userTopic
-    WHERE user_is=v_user_id AND topic_id=v_topic_id
+    WHERE user_id = v_user_id AND topic_id = v_topic_id
     INTO v_user_is_member;
     
-    IF v_user_is_member=0 THEN
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'USer is not a member of that topic';
-	END IF;
+    IF v_user_is_member = 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User is not a member of that topic';
+    END IF;
     
     INSERT INTO posts (post_date, post_title, post_content, user_id, topic_id)
-    VALUES (now(), p_post_title, p_post_content, v_user_id, v_topic_id);
+    VALUES (NOW(), p_post_title, p_post_content, v_user_id, v_topic_id);
 END //
-DELMITER ;
+DELIMITER ;
+
